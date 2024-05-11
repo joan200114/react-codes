994894ca8b91aebd5f4eebd0e41bd91810489bbc
Joan Obediente <joanobediente142001@gmail.com>
2024-05-10 22:13:47 -1200

    Add display-ptc2 feature

---
+++ display-ptc2/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div classname="App">
+      <h1>Pateros Technological College</h1>
+      <p>Introduction to Application Development</p>
+    </div>
+  );
+}
+
+export default App;
